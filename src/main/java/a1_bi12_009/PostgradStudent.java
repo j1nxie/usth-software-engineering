e2091d987ec99a08f6d19f6cddb6c0746f6482d2
j1nxie <rylieeeeexd@gmail.com>
2023-04-24 00:02:57 +0700

    docs: write javadoc for all classes and methods
    why.

--- src/main/java/a1_bi12_009/PostgradStudent.java
+++ src/main/java/a1_bi12_009/PostgradStudent.java
@@ -6,10 +6,44 @@ import utils.DomainConstraint;
 import utils.NotPossibleException;
 import utils.OptType;
 
+// @formatter:off
+/**
+ * @overview
+ *  A subclass of Student.
+ *
+ * @attributes
+ *  id            int
+ *  name          String
+ *  phoneNumber   String
+ *  address       String
+ *  gpa           float
+ *
+ * @object
+ *  An object is of form <pre>s = <i, n, p, a, g></pre>, where
+ *  <pre>id (i), name (n), phoneNumber (p), address (a), gpa (g)</pre>.
+ *
+ * @abstract_properties
+ *  P_Student | min(id) = 100000001
+ *  mutable(gpa) = true | optional(gpa) = false | min(gpa) = 0.0 | max(gpa) = 4.0
+ *
+ * @author BI12-009 Nguyen Pham Quoc An
+ **/
+// @formatter:on
 public class PostgradStudent extends Student {
 	@DomainConstraint(type = "float", mutable = true, optional = false, min = 0.0, max = 4.0)
 	protected float gpa;
 
+	// @formatter:off
+	/**
+	 * @effects
+	 *  <pre>
+	 *    if id, name, phoneNumber, address and gpa are valid
+	 *      initialize as <id, name, phoneNumber, address, gpa>
+	 *    else
+	 *      throw NotPossibleException
+	 *  </pre>
+	 **/
+	// @formatter:on
 	public PostgradStudent(@AttrRef("id") int id, @AttrRef("name") String name,
 			@AttrRef("phoneNumber") String phoneNumber, @AttrRef("address") String address, @AttrRef("float") float gpa)
 			throws NotPossibleException {
@@ -22,12 +56,32 @@ public class PostgradStudent extends Student {
 		this.gpa = gpa;
 	}
 
+	// @formatter:off
+	/**
+	 * @effects
+	 *  <pre>
+	 *    return <tt>gpa</tt>
+	 *  </pre>
+	 **/
+	// @formatter:on
 	@DOpt(type = OptType.Observer)
 	@AttrRef("gpa")
 	public float getGpa() {
 		return gpa;
 	}
 
+	// @formatter:off
+	/**
+	 * @effects
+	 *  <pre>
+	 *    if gpa is not valid
+	 *      return false
+	 *    else
+	 *      this.gpa = gpa
+	 *      return true
+	 *  </pre>
+	 **/
+	// @formatter:on
 	@DOpt(type = OptType.Mutator)
 	@AttrRef("gpa")
 	public boolean setGpa(float gpa) {
@@ -39,6 +93,17 @@ public class PostgradStudent extends Student {
 		return true;
 	}
 
+	// @formatter:off
+	/**
+	 * @effects
+	 *  <pre>
+	 *    if id is valid
+	 *      return true
+	 *    else
+	 *      return false
+	 *  </pre>
+	 **/
+	// @formatter:on
 	@Override
 	protected boolean validateId(int id) {
 		if (id < 100000001 || id > 1000000000) {
@@ -48,6 +113,17 @@ public class PostgradStudent extends Student {
 		return true;
 	}
 
+	// @formatter:off
+	/**
+	 * @effects
+	 *  <pre>
+	 *    if gpa is valid
+	 *      return true
+	 *    else
+	 *      return false
+	 *  </pre>
+	 **/
+	// @formatter:on
 	private boolean validateGpa(float gpa) {
 		if (gpa < 0.0 || gpa > 4.0) {
 			return false;
--- src/main/java/a1_bi12_009/Student.java
+++ src/main/java/a1_bi12_009/Student.java
@@ -6,8 +6,33 @@ import utils.DomainConstraint;
 import utils.NotPossibleException;
 import utils.OptType;
 
+// @formatter:off
+/**
+ * @overview
+ *  A student of an university, with two subclasses: UndergradStudent
+ *  and PostgradStudent.
+ *
+ * @attributes
+ *  id            int
+ *  name          String
+ *  phoneNumber   String
+ *  address       String
+ *
+ * @object
+ *  An object is of form <pre>s = <i, n, p, a></pre>, where
+ *  <pre>id (i), name (n), phoneNumber (p), address (a)</pre>.
+ *
+ * @abstract_properties
+ *  mutable(id) = false | optional(id) = false | min(id) = 1 | max(id) = 1000000000
+ *  mutable(name) = true | optional(name) = false | length(name) = 50
+ *  mutable(phoneNumber) = true | optional(phoneNumber) = false | length(phoneNumber) = 10
+ *  mutable(address) = true | optional(address) = false | length(address) = 100
+ *
+ * @author BI12-009 Nguyen Pham Quoc An
+ **/
+// @formatter:on
 public class Student implements Comparable<Student> {
-	@DomainConstraint(type = "int", mutable = false, optional = false, min = 1, max = 100000000)
+	@DomainConstraint(type = "int", mutable = false, optional = false, min = 1, max = 1000000000)
 	private int id;
 
 	@DomainConstraint(type = "String", mutable = true, optional = false, length = 50)
@@ -19,6 +44,17 @@ public class Student implements Comparable<Student> {
 	@DomainConstraint(type = "String", mutable = true, optional = false, length = 100)
 	private String address;
 
+	// @formatter:off
+	/**
+	 * @effects
+	 *  <pre>
+	 *    if id, name, phoneNumber and address are valid
+	 *      initialize as <id, name, phoneNumber, address>
+	 *    else
+	 *      throw NotPossibleException
+	 *  </pre>
+	 **/
+	// @formatter:on
 	public Student(@AttrRef("id") int id, @AttrRef("name") String name, @AttrRef("phoneNumber") String phoneNumber,
 			@AttrRef("address") String address) throws NotPossibleException {
 
@@ -44,30 +80,74 @@ public class Student implements Comparable<Student> {
 		this.address = address;
 	}
 
+	// @formatter:off
+	/**
+	 * @effects
+	 *  <pre>
+	 *    return <tt>id</tt>
+	 *  </pre>
+	 **/
+	// @formatter:on
 	@DOpt(type = OptType.Observer)
 	@AttrRef("id")
 	public int getId() {
 		return id;
 	}
 
+	// @formatter:off
+	/**
+	 * @effects
+	 *  <pre>
+	 *    return <tt>name</tt>
+	 *  </pre>
+	 **/
+	// @formatter:on
 	@DOpt(type = OptType.Observer)
 	@AttrRef("name")
 	public String getName() {
 		return name;
 	}
 
+	// @formatter:off
+	/**
+	 * @effects
+	 *  <pre>
+	 *    return <tt>phoneNumber</tt>
+	 *  </pre>
+	 **/
+	// @formatter:on
 	@DOpt(type = OptType.Observer)
 	@AttrRef("phoneNumber")
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
 
+	// @formatter:off
+	/**
+	 * @effects
+	 *  <pre>
+	 *    return <tt>address</tt>
+	 *  </pre>
+	 **/
+	// @formatter:on
 	@DOpt(type = OptType.Observer)
 	@AttrRef("address")
 	public String getAddress() {
 		return address;
 	}
 
+	// @formatter:off
+	/**
+	 * @effects
+	 *  <pre>
+	 *    if name is not valid
+	 *      return false
+	 *    else
+	 *      this.name = name
+	 *      return true
+	 *  </pre>
+	 **/
+	// @formatter:on
 	@DOpt(type = OptType.Mutator)
 	@AttrRef("name")
 	public boolean setName(String name) {
@@ -79,6 +159,18 @@ public class Student implements Comparable<Student> {
 		return true;
 	}
 
+	// @formatter:off
+	/**
+	 * @effects
+	 *  <pre>
+	 *    if phoneNumber is not valid
+	 *      return false
+	 *    else
+	 *      this.phoneNumber = phoneNumber
+	 *      return true
+	 *  </pre>
+	 **/
+	// @formatter:on
 	@DOpt(type = OptType.Mutator)
 	@AttrRef("phoneNumber")
 	public boolean setPhoneNumber(String phoneNumber) {
@@ -90,6 +182,18 @@ public class Student implements Comparable<Student> {
 		return true;
 	}
 
+	// @formatter:off
+	/**
+	 * @effects
+	 *  <pre>
+	 *    if address is not valid
+	 *      return false
+	 *    else
+	 *      this.address = address
+	 *      return true
+	 *  </pre>
+	 **/
+	// @formatter:on
 	@DOpt(type = OptType.Mutator)
 	@AttrRef("address")
 	public boolean setAddress(String address) {
@@ -101,6 +205,17 @@ public class Student implements Comparable<Student> {
 		return true;
 	}
 
+	// @formatter:off
+	/**
+	 * @effects
+	 *  <pre>
+	 *    if id is valid
+	 *      return true
+	 *    else
+	 *      return false
+	 *  </pre>
+	 **/
+	// @formatter:on
 	protected boolean validateId(int id) {
 		if (id < 1 || id > 1000000000) {
 			return false;
@@ -109,6 +224,17 @@ public class Student implements Comparable<Student> {
 		return true;
 	}
 
+	// @formatter:off
+	/**
+	 * @effects
+	 *  <pre>
+	 *    if name is valid
+	 *      return true
+	 *    else
+	 *      return false
+	 *  </pre>
+	 **/
+	// @formatter:on
 	protected boolean validateName(String name) {
 		if (name.length() > 50) {
 			return false;
@@ -117,6 +243,17 @@ public class Student implements Comparable<Student> {
 		return true;
 	}
 
+	// @formatter:off
+	/**
+	 * @effects
+	 *  <pre>
+	 *    if phoneNumber is valid
+	 *      return true
+	 *    else
+	 *      return false
+	 *  </pre>
+	 **/
+	// @formatter:on
 	protected boolean validatePhoneNumber(String phoneNumber) {
 		if (phoneNumber.length() > 10) {
 			return false;
@@ -125,6 +262,17 @@ public class Student implements Comparable<Student> {
 		return true;
 	}
 
+	// @formatter:off
+	/**
+	 * @effects
+	 *  <pre>
+	 *    if address is valid
+	 *      return true
+	 *    else
+	 *      return false
+	 *  </pre>
+	 **/
+	// @formatter:on
 	protected boolean validateAddress(String address) {
 		if (address.length() > 100) {
 			return false;
@@ -133,10 +281,32 @@ public class Student implements Comparable<Student> {
 		return true;
 	}
 
+	// @formatter:off
+	/**
+	 * @effects
+	 *  <pre>
+	 *    if everything is valid w.r.t. abstract properties
+	 *      return true
+	 *    else
+	 *      return false
+	 *  </pre>
+	 **/
+	// @formatter:on
 	public boolean repOK() {
 		return validateId(id) && validateName(name) && validatePhoneNumber(phoneNumber) && validateAddress(address);
 	}
 
+	// @formatter:off
+	/**
+	 * @effects
+	 *  <pre>
+	 *    if other is null
+	 *      throw NullPointerException
+	 *    else
+	 *      return this.name.compareTo(other.name)
+	 *  </pre>
+	 **/
+	// @formatter:on
 	@Override
 	public int compareTo(Student other) throws NullPointerException {
 		if (other == null) {
